e9a5548acababe42af00743560de637e9f0a3026
rafo18 <rafaelbarreto10@hotmail.com>
2023-04-28 16:19:33 -0400

    gifs_app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { ShareModule } from './shared/share.module';
+import { GifsModule } from './gifs/gifs.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ShareModule,
+    GifsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
